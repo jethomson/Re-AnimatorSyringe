95b025972eee08d4c6af0ba3c250003729d4cff1
jethomson <jonathan_e_thomson@yahoo.com>
2023-10-16 23:29:55 -0400

    Add atmega328p and fix PWM flicker for F_CPU = 1 MHz.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <Arduino.h>
 #include <EEPROM.h>
 
+// a single latching button is used to power on/off and select the effect.
+// when the uc boots the last effect used is read from the EEPROM and the next effect is determined from that.
+// max EEPROM size for attiny is 512.
+// setting EFFECT_ADDR_UB to 256 leaves room for other data.
+// by cycling through addresses we can wear out a cell more slowly than writing to the same address every time.
+// this should change number of possible boots from around 100,000 to (EFFECT_ADDR_UB/2)*100,000.
+#define EFFECT_ADDR_UB (EEPROM.length()/2)
+
 #define PERIOD 500
 #define TICS_PER_SEC (1000 / PERIOD)
 
@@ -12,7 +20,13 @@
 #define RAND16_SEED  1337
 uint16_t rand16seed = RAND16_SEED;
 
+#if defined(__AVR_ATmega328P__)
+int pwmPin = 3;
+#elif defined(__AVR_ATtiny85__)
 int pwmPin = 0;
+#endif
+
+
 uint8_t addr = 0;
 uint8_t max_brightness = 255;
 const uint8_t min_brightness = 2;
@@ -21,6 +35,7 @@ uint16_t tic = 0;
 #define NUM_EFFECTS 3
 enum EFFECT {NONE = 0, BREATHING = 1, FLICKER = 2};
 EFFECT effect = BREATHING;
+//EFFECT effect = NONE;
 
 LIB8STATIC uint8_t random8() {
     rand16seed = (rand16seed * FASTLED_RAND16_2053) + FASTLED_RAND16_13849;
@@ -44,7 +59,8 @@ LIB8STATIC uint8_t random8(uint8_t min, uint8_t lim) {
 }
 
 
-uint8_t finished_waiting(uint16_t interval) {
+// read a comment that micros() malfunctions at 1 MHz, but it works fine for me.
+uint8_t finished_waiting(uint32_t interval) {
     uint8_t finished = 0;
     uint32_t dt = 0;
     static uint32_t pm = micros(); // previous micros
@@ -55,6 +71,7 @@ uint8_t finished_waiting(uint16_t interval) {
     }
     return finished;
 }
+
 
 uint8_t breathing(uint16_t interval) {
     static uint8_t b = 0;
@@ -78,6 +95,7 @@ uint8_t breathing(uint16_t interval) {
     return b;
 }
 
+
 uint8_t flicker(uint16_t interval) {
   static uint8_t b = max_brightness;
     if (finished_waiting(interval)) {
@@ -87,31 +105,101 @@ uint8_t flicker(uint16_t interval) {
 }
 
 
-void setup() {
-    uint8_t e = EEPROM.read(addr);
+void get_effect() {
+    uint8_t e = 0;
+    uint16_t addr = 0;
+    for (; addr < EFFECT_ADDR_UB; addr++) {
+        uint8_t d = EEPROM.read(addr);
+        // the default value of a byte in an erased EEPROM is 255
+        // anything less than the 255 indicates datum was written to this address
+        // the data are stored in an inverted form so they are closer to 255
+        // which means fewer bits need to be change when setting an address back to the default (255)
+        if (d < 255) {
+           e = (~d) - 1; // e.g. (~254) - 1 = 0
+           break; 
+        }
+    }
+
+    if (addr == EFFECT_ADDR_UB) {
+        // no data was found so use default values
+        e = 0;
+        addr = 0;
+    }
+
     e = (e+1) % NUM_EFFECTS;
-    EEPROM.write(addr, e);
     effect = (EFFECT)e;
 
+    EEPROM.update(addr, 255); // update only writes the value if it differs from the stored value
+    EEPROM.write((addr+1) % EFFECT_ADDR_UB, (~e) - 1);
+}
+
+// sanity check after change clock frequency
+//void blink_timing_debug01() {
+//    digitalWrite(pwmPin, HIGH);
+//    delay(1000);
+//    digitalWrite(pwmPin, LOW);
+//    delay(1000);
+//}
+
+// sanity check after change clock frequency
+// make sure our time elapsed function still works
+//void blink_timing_debug02() {
+//    // 1000000 microseconds is 1 second
+//    if (finished_waiting(1000000)) {
+//        digitalWrite(pwmPin, !digitalRead(pwmPin));
+//    }
+//}
+
+
+void setup() {
+    //Serial.begin(4800); // cannot have a faster baud rate at 1 MHz
+    //Serial.println(EFFECT_ADDR_UB, DEC);
+
+    get_effect();
     pinMode(pwmPin, OUTPUT);
     max_brightness = 255;
+
+    //https://playground.arduino.cc/Main/TimerPWMCheatsheet/
+    //Pins 11 and 3: controlled by timer 2 in phase-correct PWM mode (cycle length = 510)
+    TCCR2B = (TCCR2B & 0b11111000) | 0x01; // divisor of 1 at 1 MHz = PWM frequency of 1960.784375 Hz
+
+    //TODO: can we make this code work for attiny85 too? different timer.
+
+    //uint8_t run_debug_num = 2;
+    //while(run_debug_num > 0) {
+    //    if (run_debug_num == 1) {
+    //        blink_timing_debug01();
+    //    }
+    //    else {
+    //        blink_timing_debug02();
+    //    }
+    //}
 }
 
 void loop() {
-    uint8_t b = 255;
+    static uint8_t b_prev = 0;
+    uint8_t b = max_brightness;
 
     switch(effect) {
         default:
         // fall through to next case
         case NONE:
+            // running at 1MHz results in a lower PWM frequency. slow enough that the duty cycle causes visible flicker
+            // a value of 32 will cause flicker if the timer's divisor is left at 64
+            //b = 32; // for PWM flicker debugging
             b = max_brightness;
             break;
         case BREATHING:
-            b = breathing(500);
+            b = breathing(4000);
             break;
         case FLICKER:
-            b = flicker(6000);
+            b = flicker(48000);
             break;
     }
-    analogWrite(pwmPin, b);
+
+    if (b != b_prev) {
+        // only write if the value is new
+        b_prev = b;
+        analogWrite(pwmPin, b);
+    }
 }
